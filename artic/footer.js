758a6a1093143827fb6d78c5b099fcc007dd55b0
Farag  مطور <bdallhfraj51@gmail.com>
2025-11-30 23:12:09 +0200

    Update footer.js

--- artic/footer.js
+++ artic/footer.js
@@ -7,10 +7,6 @@ document.addEventListener("DOMContentLoaded", function () {
   footer.style.color = "white";
   footer.style.fontSize = "0.95rem";
   footer.innerHTML = `
-    جميع الحقوق محفوظة © 2025 FaragDev98<br>
-    <a href="terms.html" style="color: #fff; text-decoration: underline;">الشروط والأحكام</a> |
-    <a href="privacy-policy.html" style="color: #fff; text-decoration: underline;">سياسة الخصوصية</a> |
-    <a href="protection.html" style="color: #fff; text-decoration: underline;">صفحة الحماية</a>
-  `;
+    جميع الحقوق محفوظة © 2025 
   document.body.appendChild(footer);
 });
