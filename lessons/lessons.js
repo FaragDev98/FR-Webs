9d73e837e1295375a428e217cc688967691a9fd6
Farag  مطور <bdallhfraj51@gmail.com>
2025-12-02 23:27:19 +0200

    Create lessons.js

---
+++ lessons/lessons.js
@@ -0,0 +1,14 @@
+function addComment() {
+  const input = document.getElementById("commentInput");
+  const commentText = input.value.trim();
+
+  if (commentText === "") return;
+
+  const commentBox = document.createElement("div");
+  commentBox.className = "comment";
+  commentBox.innerText = commentText;
+
+  document.getElementById("commentsList").appendChild(commentBox);
+
+  input.value = "";
+}
