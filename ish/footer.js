e82b5375ab37e5377830f15a65708fbac2f677f3
Farag  مطور <bdallhfraj51@gmail.com>
2025-12-07 21:40:30 +0200

    Update footer.js

--- ish/footer.js
+++ ish/footer.js
@@ -32,11 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
   ============================================================ */
   if (!document.getElementById('localMobileNav')) {
     const localNavHTML = `
-      <nav id="localMobileNav" class="nav mobile-nav mobile-hidden" aria-label="قائمة الجوال">
-        <a href="dev-english/index.html"><i class="fa-solid fa-language"></i> الإنجليزي</a>
-        <a href="artic/index.html"><i class="fa-solid fa-newspaper"></i> المقالات</a>
-        <a href="lessons-page/index.html"><i class="fa-solid fa-graduation-cap"></i> الكورسات</a>
-      </nav>
+     
     `;
     // أدخل القائمة بعد الهيدر مباشرة (أو في بداية الـ body إذا الهيدر لم يتغير)
     const headerEl = document.querySelector('header.header');
