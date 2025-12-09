260ad00d3c248fe9a7a3ff2fd56bffef7b12d12d
Farag  مطور <bdallhfraj51@gmail.com>
2025-12-09 20:45:26 +0200

    Update footer.js

--- footer.js
+++ footer.js
@@ -51,7 +51,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const bottomNavHTML = `
       <nav class="bottom-nav mobile-only" aria-label="تنقل أسفل الصفحة">
         <a href="index.html"><i class="fa-solid fa-house"></i><span>الرئيسية</span></a>
-        <a href="lessons/lessons.html"><i class="fa-solid fa-book"></i><span>الدروس</span></a>
+        <a href="lessons/FR.html"><i class="fa-solid fa-book"></i><span>الدروس</span></a>
         <button id="themeToggle" class="btn-icon" aria-label="تبديل الوضع">🌙</button>
         <a href="code/index.html"><i class="fa-solid fa-flask"></i><span>المختبر</span></a>
         <a href="contac/contact.html"><i class="fa-solid fa-phone"></i><span>تواصل</span></a>
