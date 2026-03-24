6d93b6f89a2e8479b91273a15915e7b1159122a2
Farag  مطور <bdallhfraj51@gmail.com>
2026-03-24 18:12:30 +0200

    Update footer.js

--- AB/footer.js
+++ AB/footer.js
@@ -53,7 +53,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const bottomNavHTML = `
       <nav class="bottom-nav mobile-only" aria-label="تنقل أسفل الصفحة">
         <a href="../AB/index.html"><i class="fa-solid fa-house"></i><span>الرئيسية</span></a>
-        <a href="wab/aldirs.html"><i class="fa-solid fa-user"></i><span>حسابي</span></a>
+        <a href="/wab/aldirs.html"><i class="fa-solid fa-user"></i><span>حسابي</span></a>
         <button id="themeToggle" class="btn-icon" aria-label="تبديل الوضع">🌙</button>
         <a href="/code/index.html"><i class="fa-solid fa-flask"></i><span>المختبر</span></a>
         <a href="/contac/contact.html"><i class="fa-solid fa-phone"></i><span>تواصل</span></a>
